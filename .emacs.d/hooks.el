b969de70875931b0066e14ce0ab175f90e2ae9e4
Matthew Boston <matthew.boston@gmail.com>
2012-02-16 18:11:47 -0500

    ruby and javascript are non lisp paredit

--- .emacs.d/hooks.el
+++ .emacs.d/hooks.el
@@ -1,1 +1,12 @@
 ;;(add-hook 'ruby-mode-hook 'whitespace-mode)
+
+;; ruby
+(eval-after-load 'ruby-mode
+  '(ignore-errors
+     (add-hook 'ruby-mode-hook 'esk-paredit-nonlisp)
+     (inf-ruby-keys)))
+
+;; javascript
+(eval-after-load 'javascript-mode
+  '(ignore-errors
+     (add-hook 'javascript-mode-hook 'esk-paredit-nonlisp)))
