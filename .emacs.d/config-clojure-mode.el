1ab8ffaf62c72ade7875047419b6d5f6a076ea75
Matthew Boston <matthew.boston@gmail.com>
2012-03-23 14:35:29 -0400

    move clojure-jack-in key binding to config-clojure-mode

--- .emacs.d/config-clojure-mode.el
+++ .emacs.d/config-clojure-mode.el
@@ -1,3 +1,6 @@
+;; stark swank server and open a slime session inside leiningen project
+(global-set-key (kbd "C-c C-j") 'clojure-jack-in)
+
 ;; use this function to open a repl on heroku deployed apps
 (defun heroku-repl ()
   (interactive)
