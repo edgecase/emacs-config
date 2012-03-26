d9f05ca997c2b20e55e500df9442d32882480552
Matthew Boston <matthew.boston@gmail.com>
2012-03-26 17:36:53 -0400

    function for defining a clojure function in clojure-mode

--- .emacs.d/config-clojure-mode.el
+++ .emacs.d/config-clojure-mode.el
@@ -1,7 +1,16 @@
 ;; stark swank server and open a slime session inside leiningen project
-(global-set-key (kbd "C-c C-j") 'clojure-jack-in)
+(define-key clojure-mode-map (kbd "C-c C-j") 'clojure-jack-in)
 
 ;; use this function to open a repl on heroku deployed apps
 (defun heroku-repl ()
   (interactive)
   (inferior-lisp "heroku run lein repl"))
+
+(defun define-function ()
+  (interactive)
+  (let ((name (symbol-at-point)))
+    (backward-paragraph)
+    (insert "\n(defn " (symbol-name name) "\n  \"\"\n  []\n  )\n")
+    (backward-char 11)))
+
+(define-key clojure-mode-map (kbd "C-c f") 'define-function)
