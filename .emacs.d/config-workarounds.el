c5baf980f89bbd0aecc18bea4628229c5f096e35
Matthew Boston <matthew.boston@gmail.com>
2012-05-03 11:54:30 -0400

    add a hook which turns off long line highlighting for markdown-mode

--- .emacs.d/config-workarounds.el
+++ .emacs.d/config-workarounds.el
@@ -1,2 +1,6 @@
 ;; come on guys; autoloads are not rocket science
 (autoload 'marmalade-upload-buffer "marmalade" nil t)
+
+;; modes which should not have highlighted long lines
+(add-hook 'markdown-mode-hook
+          (setq whitespace-style nil))
