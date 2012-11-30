72e08887130839b10366689680017b749b77d898
Matthew Boston <matthew.boston@gmail.com>
2012-11-30 14:18:23 -0500

    update to use newest version of yasnippet

--- .emacs.d/config-yasnippet.el
+++ .emacs.d/config-yasnippet.el
@@ -1,4 +1,2 @@
 (require 'yasnippet)
-(yas/initialize)
-(setq yas/root-directory "~/.emacs.d/snippets")
-(yas/load-directory yas/root-directory)
+(yas-global-mode 1)
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,6 +4,7 @@
 
 (require 'package)
 (add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/")
              '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
 
